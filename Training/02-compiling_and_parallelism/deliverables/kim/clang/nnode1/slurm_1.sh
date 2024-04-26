96f24a0c65048e220c2b9b726cff48466733389b
Kim Takala <kitakita@mahti-login11.mahti.csc.fi>
2024-04-26 11:45:12 +0300

    added kim

---
+++ Training/02-compiling_and_parallelism/deliverables/kim/clang/nnode1/slurm_1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --account=project_2009905
+#SBATCH --job-name=heat_mpi_kim4
+#SBATCH --time=0:01:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=128
+#SBATCH --partition=test
+
+srun /scratch/project_2009905/kitakita/heat-equation/3d/mpi/heat_mpi
---
+++ Training/02-compiling_and_parallelism/deliverables/kim/clang/nnode4/slurm_4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --account=project_2009905
+#SBATCH --job-name=heat_mpi_kim6
+#SBATCH --time=0:01:00
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=128
+#SBATCH --partition=medium
+
+srun /scratch/project_2009905/kitakita/heat-equation/3d/mpi/heat_mpi
