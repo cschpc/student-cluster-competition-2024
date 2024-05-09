de4d2bb45b9f83e4015d6a66d0a2decb694b482a
Kim Takala <kitakita@mahti-login12.mahti.csc.fi>
2024-05-09 13:58:52 +0300

    Added löyly

---
+++ Training/03-threading_and_hpl/deliverables/Team_Löyly/kitakita/64/slurm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --account=project_2009905
+#SBATCH --job-name=HPL_KIM
+#SBATCH --time=0:10:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=test
+
+srun xhpl
